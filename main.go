219ff1241e904a20d394abde334107d1a3abf591
Jan Kåre Vatne <jkvatne@online.no>
2025-02-20 11:23:57 +0100

    Showing triangle

--- main.go
+++ main.go
@@ -6,47 +6,97 @@ import (
 	"github.com/go-gl/glfw/v3.3/glfw"
 	"log"
 	"runtime"
+	"strings"
 	"testglfont/glfont"
-
-	// "testglfont/glfont"
 )
 
-const windowWidth = 2300
-const windowHeight = 1200
+const (
+	vertexShaderSource = `
+		#version 400
 
-var oldState glfw.Action
-var triangle = []float32{
-	0, 0.5, 0, // top
-	-0.5, -0.5, 0, // left
-	0.5, -0.5, 0, // right
+		in vec3 vp;
+		void main() {
+			gl_Position = vec4(vp, 1.0);
+		}
+	` + "\x00"
+
+	fragmentShaderSource = `
+		#version 400
+
+		out vec4 frag_colour;
+		void main() {
+  			frag_colour = vec4(1, 1, 1, 1.0);
+		}
+	` + "\x00"
+	windowWidth  = 2300
+	windowHeight = 1200
+)
+
+var vao uint32
+
+// https://github.com/go-gl/examples/blob/master/gl41core-cube/cube.go
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+		infoLog := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(infoLog))
+		return 0, fmt.Errorf("failed to compile %v: %v", source, infoLog)
+	}
+	return shader, nil
 }
 
-func init() {
-	runtime.LockOSThread()
-}
-
-func CreateWindow() *glfw.Window {
-	glfw.WindowHint(glfw.Resizable, glfw.True)
-	glfw.WindowHint(glfw.ContextVersionMajor, 3)
-	glfw.WindowHint(glfw.ContextVersionMinor, 2)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-	glfw.WindowHint(glfw.Floating, glfw.True)
-	window, _ := glfw.CreateWindow(int(windowWidth), int(windowHeight), "Font demo", nil, nil)
-	window.MakeContextCurrent()
-	glfw.SwapInterval(1)
-	scaleX, scaleY := window.GetContentScale()
-	log.Printf("Window scaleX=%v, scaleY=%v\n", scaleX, scaleY)
-	return window
+// makeVao initializes and returns a vertex array from the points provided.
+func makeVao(points []float32) {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
 }
 
 // https://www.glfw.org/docs/latest/window_guide.html
 func KeyCallback(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
-	log.Println("%v %v %v %v\n", key, scancode, action, mods)
+	log.Printf("Key %v %v %v %v\n", key, scancode, action, mods)
 }
 
 func InitKeys(window *glfw.Window) {
 	window.SetKeyCallback(KeyCallback)
+}
+
+// initGlfw initializes glfw and returns a Window to use.
+func initGlfw(width, height int, name string) *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.False)
+	glfw.WindowHint(glfw.Floating, glfw.False) // Will keep window on top if true
+
+	window, err := glfw.CreateWindow(width, height, name, nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+	glfw.SwapInterval(1)
+	scaleX, scaleY := window.GetContentScale()
+	log.Printf("Window scaleX=%v, scaleY=%v\n", scaleX, scaleY)
+
+	return window
 }
 
 // initOpenGL initializes OpenGL and returns an intiialized program.
@@ -57,30 +107,87 @@ func initOpenGL() uint32 {
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	log.Println("OpenGL version", version)
 
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
 	gl.Enable(gl.DEPTH_TEST)
 	gl.DepthFunc(gl.LESS)
-	gl.ClearColor(1.0, 1.0, 1.0, 1.0)
+	gl.ClearColor(0.95, 0.95, 0.86, 1.0)
 
+	SetupTransform(windowWidth, windowHeight)
 	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
 	gl.LinkProgram(prog)
 	return prog
+}
+
+// Because Gil specified "screen coordinates" (presumably with an upper-left origin), this short bit of
+// code sets up the coordinate system to correspond to actual window coodrinates.  This code
+// wouldn't be required if you chose a (more typical in 3D) abstract coordinate system.
+func SetupTransform(w float64, h float64) {
+	// Establish viewing area to cover entire window.
+	gl.Viewport(0, 0, int32(w), int32(h))
+	// Start modifying the projection matrix.
+	gl.MatrixMode(gl.PROJECTION)
+	// Reset project matrix.
+	gl.LoadIdentity()
+	// Map abstract coords directly to window coords
+	gl.Ortho(0, w, 0, h, -1, 1)
+	// Invert Y axis so increasing Y goes down.
+	gl.Scalef(1, -1, 1)
+	// Shift origin up to upper-left corner
+	gl.Translatef(0, float32(-h), 0)
+}
+
+func drawTriangle() {
+	gl.Begin(gl.TRIANGLES)
+	gl.Color3f(0.0, 0.0, 1.0) /* blue */
+	gl.Vertex2i(-10, -10)
+	gl.Color3f(0.0, 1.0, 0.0) /* green */
+	gl.Vertex2i(100, 100)
+	gl.Color3f(1.0, 0.0, 0.0) /* red */
+	gl.Vertex2i(20, -100)
+	gl.End()
 }
 
 func draw(window *glfw.Window, prog uint32) {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 	gl.UseProgram(prog)
+	// Do actual drawing
+	// drawTriangle()
+	gl.BindVertexArray(vao)
+	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle)/3))
 
-	glfw.PollEvents()
-	window.SwapBuffers()
+}
+
+var font *glfont.Font
+
+func LoadFonts() {
+	var err error
+	font, err = glfont.LoadFont("Roboto-Medium.ttf", 35, windowWidth, windowHeight)
+	if err != nil {
+		log.Panicf("LoadFont: %v", err)
+	}
+}
+
+var triangle = []float32{
+	0, 0.5, 0,
+	-0.5, -0.5, 0,
+	0.5, -0.5, 0,
 }
 
 func main() {
-
-	if err := glfw.Init(); err != nil {
-		log.Fatalln("failed to initialize glfw:", err)
-	}
+	runtime.LockOSThread()
+	window := initGlfw(windowWidth, windowHeight, "Demo")
 	defer glfw.Terminate()
-
 	monitors := glfw.GetMonitors()
 	for i, monitor := range monitors {
 		mw, mh := monitor.GetPhysicalSize()
@@ -91,49 +198,23 @@ func main() {
 		log.Printf("Monitor %d, %vmmx%vmm, %vx%vpx,  pos: %v, %v\n", i+1, mw, mh, w, h, x, y)
 	}
 
-	window := CreateWindow()
-	_ = initOpenGL()
+	prog := initOpenGL()
+	makeVao(triangle)
 
-	font, err := glfont.LoadFont("Roboto-Medium.ttf", 35, windowWidth, windowHeight)
-	if err != nil {
-		log.Panicf("LoadFont: %v", err)
-	}
-
+	LoadFonts()
 	InitKeys(window)
-
-	// prog := initOpenGL()
-	gl.MatrixMode(gl.PROJECTION)
-	gl.LoadIdentity()
-	gl.Ortho(0.0, windowWidth, 0.0, windowHeight, -1.0, 1.0)
-	gl.MatrixMode(gl.MODELVIEW)
 
 	for !window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-		gl.Color3f(1.0, 0.0, 1.0)
-		gl.Begin(gl.POLYGON)
-		gl.Vertex2i(-50, -90)
-		gl.Vertex2i(100, -90)
-		gl.Vertex2i(100, 150)
-		gl.Vertex2i(-50, 150)
-		gl.End()
-		gl.Flush()
-
+		draw(window, prog)
 		// FPS=3 for 100*22*16=35200 labels! Dvs 10000 tests pr sec
 		// set color and draw text
-		font.SetColor(0.0, 0.0, 0.0, 1.0)
+		// font.SetColor(0.0, 0.0, 0.0, 1.0)
 		// _ = font.Printf(float32((i&0xF)*120+10), float32(25+(i>>4)*50), 1.0, "Aøæ©")
-		font.Printf(10, 50, 1.0, "Hello World!")
-
+		// font.Printf(10, 50, 1.0, "Hello World!")
 		window.SwapBuffers()
 		glfw.PollEvents()
-
-		// fmt.Printf("FPS: %v\r", 1.0/time.Since(t).Seconds())
-		state := window.GetKey(glfw.KeyA)
-		if state != oldState {
-			fmt.Printf("Escape %v\n", state)
-			oldState = state
-		}
-		oldState = state
 	}
+	glfw.Terminate()
+
 }
