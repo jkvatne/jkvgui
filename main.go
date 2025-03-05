cf7d9676ae5d9f1c49ad1f9bdc29f8df4419d19d
Jan Kåre Vatne <jkvatne@online.no>
2025-03-05 10:03:08 +0100

    Edit with static content

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ func NoBtnClick() {
 	log.Printf("No Btn Click\n")
 }
 
+var name = "jkvgui"
+
 func Form() wid.Wid {
 	r := wid.RowSetup{}
 	w := wid.Row(r,
@@ -29,6 +31,7 @@ func Form() wid.Wid {
 		wid.Button("Cancel", CancelBtnClick, wid.OkBtn),
 		wid.Button("No", NoBtnClick, wid.OkBtn),
 		wid.Button("Yes", YesBtnClick, wid.OkBtn),
+		wid.Edit(&name, 10, nil, wid.DefaultEdit),
 	)
 	return w
 }
@@ -47,13 +50,13 @@ func main() {
 	for !window.ShouldClose() {
 		gpu.StartFrame()
 		gpu.RoundedRect(300, 50, 100, 100, 10, 2, gpu.Lightgrey, gpu.Blue)
-		gpu.Fonts[0].Printf(50, 100, 24, "24 Roboto100")
-		gpu.Fonts[1].Printf(50, 130, 24, "24 Roboto200")
-		gpu.Fonts[2].Printf(50, 160, 24, "24 Roboto300")
-		gpu.Fonts[3].Printf(50, 190, 24, "24 Roboto400")
-		gpu.Fonts[4].Printf(50, 220, 24, "24 Roboto500")
-		gpu.Fonts[5].Printf(50, 250, 24, "24 Roboto600")
-		gpu.Fonts[6].Printf(50, 280, 24, "24 Roboto700")
+		gpu.Fonts[2].Printf(50, 100, 24, "24 Roboto100")
+		gpu.Fonts[3].Printf(50, 130, 24, "24 Roboto200")
+		gpu.Fonts[4].Printf(50, 160, 24, "24 Roboto300")
+		gpu.Fonts[5].Printf(50, 190, 24, "24 Roboto400")
+		gpu.Fonts[0].Printf(50, 220, 24, "24 Roboto500")
+		gpu.Fonts[6].Printf(50, 250, 24, "24 Roboto600")
+		gpu.Fonts[1].Printf(50, 280, 24, "24 Roboto700")
 		gpu.Fonts[7].Printf(50, 310, 24, "24 Roboto800")
 		// Red frame around the whole window
 		gpu.Rect(10, 10, float32(gpu.WindowWidthDp)-20, float32(gpu.WindowHeightDp)-20, 2, gpu.Transparent, gpu.Red)
---
+++ wid/edit.go
@@ -0,0 +1,72 @@
+package wid
+
+import "github.com/jkvatne/jkvgui/gpu"
+
+type EditStyle struct {
+	FontSize           float32
+	FontNo             int
+	FontColor          gpu.Color
+	InsideColor        gpu.Color
+	BorderColor        gpu.Color
+	BorderWidth        float32
+	BorderCornerRadius float32
+	InsidePadding      Padding
+	OutsidePadding     Padding
+	CursorWidth        float32
+}
+
+var DefaultEdit = EditStyle{
+	FontSize:           16,
+	FontNo:             0,
+	InsideColor:        gpu.Color{0.9, 0.9, 0.9, 1.0},
+	BorderColor:        gpu.Color{0, 0, 0, 1},
+	FontColor:          gpu.Color{0, 0, 0, 1},
+	OutsidePadding:     Padding{5, 5, 5, 5},
+	InsidePadding:      Padding{8, 5, 5, 5},
+	BorderWidth:        1,
+	BorderCornerRadius: 5,
+	CursorWidth:        1,
+}
+
+type EditState struct {
+	SelStart int
+	SelEnd   int
+	Buffer   string
+}
+
+var StateMap = make(map[*string]*EditState)
+
+func Edit(text *string, size int, action func(), style EditStyle) Wid {
+	return func(ctx Ctx) Dim {
+		s := StateMap[text]
+		if s == nil {
+			StateMap[text] = &EditState{}
+			s = StateMap[text]
+			s.Buffer = *text
+		}
+		scale := style.FontSize / gpu.InitialSize
+		dho := style.OutsidePadding.T + style.OutsidePadding.B
+		dhi := style.InsidePadding.T + style.InsidePadding.B + 2*style.BorderWidth
+		dwi := style.InsidePadding.L + style.InsidePadding.R + 2*style.BorderWidth
+		dwo := style.OutsidePadding.R + style.OutsidePadding.L
+		height := (gpu.Fonts[style.FontNo].Ascent+gpu.Fonts[style.FontNo].Descent)*scale + dho + dhi
+		width := float32(size)*gpu.Fonts[style.FontNo].Width(style.FontSize, "n")/gpu.InitialSize + dwo + dwi
+		baseline := gpu.Fonts[style.FontNo].Ascent*scale + style.OutsidePadding.T + style.InsidePadding.T + style.BorderWidth
+		s.Buffer = *text
+		if ctx.Rect.H == 0 {
+			return Dim{w: width, h: height, baseline: baseline}
+		}
+		gpu.RoundedRect(
+			ctx.Rect.X+style.OutsidePadding.L,
+			ctx.Rect.Y+style.OutsidePadding.T,
+			width-style.OutsidePadding.L-style.OutsidePadding.R,
+			height-style.OutsidePadding.T-style.OutsidePadding.B,
+			style.BorderCornerRadius, style.BorderWidth, style.InsideColor, style.BorderColor)
+		gpu.Fonts[style.FontNo].SetColor(style.FontColor)
+		gpu.Fonts[style.FontNo].Printf(
+			ctx.Rect.X+style.OutsidePadding.L+style.InsidePadding.L+style.BorderWidth,
+			ctx.Rect.Y+baseline,
+			style.FontSize, *text)
+		return Dim{w: width, h: height, baseline: baseline}
+	}
+}
