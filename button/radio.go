37c35cc6fca4cab32d5a16de42c20b3a3acefb7a
Jan Kåre Vatne <jkvatne@online.no>
2025-03-27 13:16:18 +0100

    Fix radio button

--- button/radio.go
+++ button/radio.go
@@ -60,6 +60,7 @@ func RadioButton(text string, value *string, key string, style *RadioButtonStyle
 		} else {
 			icon.Draw(iconRect.X, iconRect.Y-1, iconRect.H, icon.RadioUnchecked, style.Role.Fg())
 		}
+		f.SetColor(style.Role.Fg())
 		f.Printf(iconRect.X+fontHeight*6/5, ctx.Rect.Y+baseline, style.FontSize, 0, text)
 
 		return wid.Dim{W: ctx.Rect.W, H: ctx.Rect.H, Baseline: ctx.Baseline}
