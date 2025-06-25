cdc40dbea82656811bad618b49a9c5a13fd75bcb
Jan Kåre Vatne <jkvatne@online.no>
2025-06-25 17:58:26 +0200

    Fix resizer demo with two windows

--- examples/resize/resize.go
+++ examples/resize/resize.go
@@ -7,12 +7,12 @@ import (
 	"github.com/jkvatne/jkvgui/wid"
 )
 
-var pos1 wid.ResizerState
-var pos2 wid.ResizerState
+var posV [16]wid.ResizerState
+var posH [16]wid.ResizerState
 
 func main() {
 	sys.CreateWindow(f32.Rect{100, 100, 400, 400}, "Resizing1", 1, 2)
-	// sys.CreateWindow(f32.Rect{400, 400, 400, 400}, "Resizing2", 2, 2)
+	sys.CreateWindow(f32.Rect{400, 400, 400, 400}, "Resizing2", 2, 2)
 	defer sys.Shutdown()
 	image, _ := wid.NewImage("music.jpg")
 	for {
@@ -20,9 +20,9 @@ func main() {
 			sys.StartFrame(wno, theme.Surface.Bg())
 			ctx := wid.NewCtx(wno)
 			wid.HorResizer(
-				&pos1, nil,
+				&posH[wno], nil,
 				wid.Image(image, nil, ""),
-				wid.VertResizer(&pos2, nil,
+				wid.VertResizer(&posV[wno], nil,
 					wid.Btn("Left", nil, func() {}, nil, ""),
 					wid.Btn("Right", nil, func() {}, nil, ""),
 				),
