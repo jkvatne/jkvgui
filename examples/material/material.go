abf6c0aee718485fa400e90fc2b45bbac6bde5af
Jan Kåre Vatne <jkvatne@online.no>
2026-01-15 08:44:32 +0100

    Restructure material demo

--- examples/material/material.go
+++ examples/material/material.go
@@ -28,30 +28,71 @@ var (
 	CacheMaxSize = 16
 	DbTotalCount int
 	MenuStyle    = (&wid.ContainerStyle{}).W(0.3)
+	// MyItemStyle is a container style for showing each record from the simulated database table
+	MyItemStyle = wid.ContainerStyle{
+		BorderRole:     theme.Outline,
+		BorderWidth:    1,
+		Role:           theme.PrimaryContainer,
+		CornerRadius:   5.0,
+		InsidePadding:  f32.Padding{L: 3, T: 3, R: 3, B: 3},
+		OutsidePadding: f32.Padding{L: 3, T: 5, R: 3, B: 5},
+	}
 )
 
-func do() {
-	slog.Info("Save clicked")
+// dbCount returns the total number of articles
+// This could be a database query for count(*)
+func dbCount() int {
+	// Simulation has items 0 to 20, for a total of 21 items.
+	return 21
 }
 
-// Menu demonstrates how to show a list that is generated while drawing it.
-func Menu() wid.Wid {
-	return wid.Col(MenuStyle,
-		wid.Label("Genre", &smallText),
-		func(ctx wid.Ctx) wid.Dim {
-			widgets := make([]wid.Wid, len(entries)+1)
-			for i, s := range entries {
-				widgets[i] = wid.Btn(s, gpu.Home, nil, wid.Text, "")
-			}
-			widgets[len(entries)] = wid.Label(fmt.Sprintf("MousePos = %5.0f, %5.0f ", sys.WindowList[0].MousePos().X, sys.WindowList[0].MousePos().Y), nil)
-			return wid.Col(wid.Secondary.W(0.3), widgets...)(ctx)
-		},
-	)
+// dbRead simulates a database query, reading the contents of an article.
+// Here we just simulate a database lookup by returning an item based on the given index n
+func dbRead(n int) wid.Wid {
+	switch n {
+	case 0:
+		return wid.Label("0 Articles "+strconv.Itoa(dbCount()), &smallText)
+	case 1:
+		return wid.Col(&MyItemStyle,
+			wid.Label("1 Hiphop", nil),
+			wid.Label("What Buttons are Artists Pushing When They Perform Live", &heading),
+			wid.Label("12 hrs ago", &smallText),
+			wid.Image(music, nil, wid.DefImg.Bg(theme.PrimaryContainer), ""),
+			wid.Row(nil,
+				wid.Flex(),
+				wid.Btn("Save", gpu.ContentSave, do, nil, ""),
+			),
+		)
+	case 2:
+		return wid.Col(&MyItemStyle,
+			wid.Label("2 More about Taylor Swift...", &heading),
+			wid.Image(swift, nil, wid.DefImg.Bg(theme.PrimaryContainer), ""),
+		)
+	case 3:
+		return wid.Col(&MyItemStyle,
+			wid.Label("3 The new Beatles...", &heading),
+		)
+	case 4, 5, 6, 7:
+		return wid.Col(&MyItemStyle,
+			wid.Label(strconv.Itoa(n)+" More about Taylor Swift...", &heading),
+			wid.Image(swift, nil, wid.DefImg.Bg(theme.PrimaryContainer), ""),
+		)
+	case 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19:
+		return wid.Col(&MyItemStyle,
+			wid.Label(strconv.Itoa(n)+" Some text here", &heading),
+		)
+	case 20:
+		return wid.Col(&MyItemStyle,
+			wid.Label(strconv.Itoa(n)+" Last item", &heading),
+		)
+	default:
+		return nil
+	}
 }
 
 // GetItem implements a cache.
 func GetItem(idx int) wid.Wid {
-	DbTotalCount = GetTotalCount()
+	DbTotalCount = dbCount()
 	if idx >= DbTotalCount {
 		return nil
 	}
@@ -97,17 +138,10 @@ func GetItem(idx int) wid.Wid {
 	return Cache[idx-CacheStart]
 }
 
-// GetTotalCount returns the total number of articles
-// This could be a database query for count(*)
-func GetTotalCount() int {
-	// Simulation has items 0 to 20, for a total of 21 items.
-	return 21
-}
-
 // GetRangeFromDb reads a number of items from the database
 func GetRangeFromDb(start int, count int) []wid.Wid {
 	var w []wid.Wid
-	DbTotalCount = GetTotalCount()
+	DbTotalCount = dbCount()
 	// slog.Debug("GetRangeFromDb", "start", start, "DbTotalCount", DbTotalCount)
 	if start >= DbTotalCount {
 		return nil
@@ -116,71 +150,33 @@ func GetRangeFromDb(start int, count int) []wid.Wid {
 		if i+start >= DbTotalCount {
 			break
 		}
-		w = append(w, getFromDb(start+i))
+		w = append(w, dbRead(start+i))
 	}
 	return w
 }
 
-// GetItems could for example be a database query, reading article n
-// Here we just simulate a database lookup by returning an item
-// based on the given index n
-func getFromDb(n int) wid.Wid {
-	switch n {
-	case 0:
-		return wid.Label("0 Articles "+strconv.Itoa(GetTotalCount()), &smallText)
-	case 1:
-		return wid.Col(&MyItemStyle,
-			wid.Label("1 Hiphop", nil),
-			wid.Label("What Buttons are Artists Pushing When They Perform Live", &heading),
-			wid.Label("12 hrs ago", &smallText),
-			wid.Image(music, nil, wid.DefImg.Bg(theme.PrimaryContainer), ""),
-			wid.Row(nil,
-				wid.Flex(),
-				wid.Btn("Save", gpu.ContentSave, do, nil, ""),
-			),
-		)
-	case 2:
-		return wid.Col(&MyItemStyle,
-			wid.Label("2 More about Taylor Swift...", &heading),
-			wid.Image(swift, nil, wid.DefImg.Bg(theme.PrimaryContainer), ""),
-		)
-	case 3:
-		return wid.Col(&MyItemStyle,
-			wid.Label("3 The new Beatles...", &heading),
-		)
-	case 4, 5, 6, 7:
-		return wid.Col(&MyItemStyle,
-			wid.Label(strconv.Itoa(n)+" More about Taylor Swift...", &heading),
-			wid.Image(swift, nil, wid.DefImg.Bg(theme.PrimaryContainer), ""),
-		)
-	case 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19:
-		return wid.Col(&MyItemStyle,
-			wid.Label(strconv.Itoa(n)+" Some text here", &heading),
-		)
-	case 20:
-		return wid.Col(&MyItemStyle,
-			wid.Label(strconv.Itoa(n)+" Last item", &heading),
-		)
-	default:
-		return nil
-	}
+func do() {
+	slog.Info("Save clicked")
 }
 
-var MyItemStyle = wid.ContainerStyle{
-	BorderRole:     theme.Outline,
-	BorderWidth:    1,
-	Role:           theme.PrimaryContainer,
-	CornerRadius:   5.0,
-	InsidePadding:  f32.Padding{L: 3, T: 3, R: 3, B: 3},
-	OutsidePadding: f32.Padding{L: 3, T: 5, R: 3, B: 5},
-}
-
-func CachedItems() wid.Wid {
-	return wid.CashedScroller(ss, &wid.DefaultScrollStyle, GetItem, GetTotalCount)
+// Menu demonstrates how to show a list that is generated while drawing it.
+func Menu() wid.Wid {
+	return wid.Col(MenuStyle,
+		wid.Label("Genre", &smallText),
+		func(ctx wid.Ctx) wid.Dim {
+			widgets := make([]wid.Wid, len(entries)+1)
+			for i, s := range entries {
+				widgets[i] = wid.Btn(s, gpu.Home, nil, wid.Text, "")
+			}
+			widgets[len(entries)] = wid.Label(fmt.Sprintf("MousePos = %5.0f, %5.0f ", sys.WindowList[0].MousePos().X, sys.WindowList[0].MousePos().Y), nil)
+			return wid.Col(wid.Secondary.W(0.3), widgets...)(ctx)
+		},
+	)
 }
 
 func Form() wid.Wid {
-	return wid.Row(MainRow, Menu(), CachedItems())
+	return wid.Row(MainRow, Menu(),
+		wid.CashedScroller(ss, &wid.DefaultScrollStyle, GetItem, dbCount))
 }
 
 func main() {
